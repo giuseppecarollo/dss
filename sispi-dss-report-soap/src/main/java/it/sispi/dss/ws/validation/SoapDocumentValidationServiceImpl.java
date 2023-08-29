6dcbdbabd1644deddea521932a85d4c5f87e04b6
Antonio Flaccomio <a.flaccomio@sispi.it>
2023-08-29 11:51:19 +0200

    sispi validator

--- sispi-dss-report-soap/src/main/java/it/sispi/dss/ws/validation/SoapDocumentValidationServiceImpl.java
+++ sispi-dss-report-soap/src/main/java/it/sispi/dss/ws/validation/SoapDocumentValidationServiceImpl.java
@@ -18,8 +18,19 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package it.sispi.dss.ws.validation.soap;
+package it.sispi.dss.ws.validation;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+import javax.xml.transform.TransformerException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.europa.esig.dss.detailedreport.DetailedReportFacade;
+import eu.europa.esig.dss.simplereport.SimpleReportFacade;
 import eu.europa.esig.dss.ws.dto.RemoteDocument;
 import eu.europa.esig.dss.ws.validation.common.RemoteDocumentValidationService;
 import eu.europa.esig.dss.ws.validation.dto.DataToValidateDTO;
@@ -27,51 +38,61 @@ import eu.europa.esig.dss.ws.validation.dto.WSReportsDTO;
 //import eu.europa.esig.dss.ws.validation.soap.client.SoapDocumentValidationService;
 import it.sispi.dss.ws.validation.soap.client.SoapDocumentValidationService;
 
-import java.util.List;
-
 /**
  * SOAP implementation of the validation service
  */
 @SuppressWarnings("serial")
 public class SoapDocumentValidationServiceImpl implements SoapDocumentValidationService {
+  
+  private static final Logger LOG = LoggerFactory.getLogger(SoapDocumentValidationServiceImpl.class);
 
-	/** The validation service to use */
-	private RemoteDocumentValidationService validationService;
+  /** The validation service to use */
+  private RemoteDocumentValidationService validationService;
 
-	/**
-	 * Default construction instantiating object with null RemoteDocumentValidationService
-	 */
-	public SoapDocumentValidationServiceImpl() {
-		// empty
-	}
+  /**
+   * Default construction instantiating object with null RemoteDocumentValidationService
+   */
+  public SoapDocumentValidationServiceImpl() {
+    // empty
+  }
 
-	/**
-	 * Default constructor
-	 *
-	 * @param validationService {@link RemoteDocumentValidationService}
-	 */
-	public void setValidationService(RemoteDocumentValidationService validationService) {
-		this.validationService = validationService;
-	}
+  /**
+   * Default constructor
+   *
+   * @param validationService {@link RemoteDocumentValidationService}
+   */
+  public void setValidationService(RemoteDocumentValidationService validationService) {
+    this.validationService = validationService;
+  }
 
-	@Override
-	public WSReportsDTO validateSignature(DataToValidateDTO dataToValidate) {
-		return validationService.validateDocument(dataToValidate);
-	}
+  @Override
+  public WSReportsDTO validateSignature(DataToValidateDTO dataToValidate) {
+    return validationService.validateDocument(dataToValidate);
+  }
 
-	@Override
-	public List<RemoteDocument> getOriginalDocuments(DataToValidateDTO dataToValidate) {
-		return validationService.getOriginalDocuments(dataToValidate);
-	}
+  @Override
+  public List<RemoteDocument> getOriginalDocuments(DataToValidateDTO dataToValidate) {
+    return validationService.getOriginalDocuments(dataToValidate);
+  }
 
-	 @Override     
-	  public String getHTMLDetailedReport(DataToValidateDTO dataToValidate) {
-	    return validationService.getHTMLDetailedReport(dataToValidate);
-	  }
+  @Override     
+  public String getHTMLDetailedReport(DataToValidateDTO dataToValidate) {
+    try {
+      return DetailedReportFacade.newFacade().generateHtmlReport(validationService.validateDocument(dataToValidate).getDetailedReport());
+    } catch (IOException | TransformerException | JAXBException e) {
+      LOG.error("getHTMLReport", e);
+      return "error";
+    }
+  }
 
-	  @Override     
-	  public String getHTMLReport(DataToValidateDTO dataToValidate) {
-	    return validationService.getHTMLReport(dataToValidate);
-	  }
+  @Override     
+  public String getHTMLReport(DataToValidateDTO dataToValidate) {
+    try {
+      return SimpleReportFacade.newFacade().generateHtmlReport(validationService.validateDocument(dataToValidate).getSimpleReport());
+    } catch (IOException | TransformerException | JAXBException e) {
+      LOG.error("getHTMLReport", e);
+      return "error";
+    }
+  }
 
 }
